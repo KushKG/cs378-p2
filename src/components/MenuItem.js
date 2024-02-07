d5b8804dada7b986ed46cca55fb946c1bd454274
Kush Gandhi <kushgandhi@wireless-10-146-37-163.public.utexas.edu>
2024-02-07 16:02:22 -0600

    Finished react app

---
+++ src/components/MenuItem.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import '../App.css';
+
+// This is a functional component that represents a single menu item. It currently takes in the title and displays it in an h2 element.
+// Modify the component to take in all the other properties of a menu item you need and display them in the component.
+// Use bootstrap to style the elements so that it looks like the mockup in the assignment.
+// Hint: You can use the image name to get the image from the images folder.
+const MenuItem = ({ title, description, price, imageName }) => {
+    return (
+    <div className="mt-3 container">
+      <div className="row justify-content-center">
+        <div className="col-4 rounded image-div">
+          <img src={`./images/${imageName}`} className="image-size image-menu" alt={title} />
+        </div>
+        <div className="col">
+          <div className="row">
+            <h5 className="text-start fw-bold">{title}</h5>
+          </div>
+          <div className="row">
+            <h6 className="fw-light">{description}</h6>
+          </div>
+          <div className="row">
+            <div className="d-flex justify-content-between">
+              {`$${price}`}
+              <p className="m-0">
+                <button className="px-3 add-button">Add</button>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    );
+};
+
+export default MenuItem;
